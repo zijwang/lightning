ab23985f7364a09269303ff6ce130127078bfb79
tchaton <thomas@grid.ai>
2021-05-06 10:20:16 +0100

    i

--- tests/mn.sh
+++ tests/mn.sh
@@ -4,3 +4,6 @@ SHA=$(git rev-parse HEAD)
 echo $SHA
 kubectl delete  ElasticJob debugging -n elastic-job
 cat mn.yaml | sed 's/%(SHA)s/$SHA/g' | kubectl create -f -
+kubectl logs test-worker-0 -n elastic-job -f
+kubectl logs test-worker-0 -n elastic-job -f
+kubectl get pods -n elastic-job -w

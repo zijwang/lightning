51ec0469b7c64089bad6820a0ca5ab9437c1a7ea
tchaton <thomas@grid.ai>
2021-05-06 10:11:41 +0100

    i

--- tests/mn.sh
+++ tests/mn.sh
@@ -2,4 +2,5 @@
 git add -A && git commit -m 'i' && git push --set-upstream origin $1
 SHA=$(git rev-parse HEAD)
 echo $SHA
+kubectl delete  ElasticJob debugging -n elastic-job
 cat mn.yaml | sed 's/%(SHA)s/$SHA/g' | kubectl create -f -

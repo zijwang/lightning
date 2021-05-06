cb56cbc75c9891770aee68449df0ae5e6da36362
tchaton <thomas@grid.ai>
2021-05-06 10:10:20 +0100

    i

--- tests/mn.sh
+++ tests/mn.sh
@@ -1,4 +1,4 @@
-git add -A && git commit -m 'i' && git push origin $1
+git add -A && git commit -m 'i' && git push --set-upstream origin $1
 SHA=$(git rev-parse HEAD)
 echo $SHA
 cat mn.yaml | sed 's/%(SHA)s/$SHA/g' | kubectl create -f -

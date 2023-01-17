243366da80bed75d955e596a98044acb529a91d5
awaelchli <aedu.waelchli@gmail.com>
2023-01-17 12:46:26 +0100

    fsdp

---
+++ examples/fabric/fsdp_1.py
@@ -0,0 +1,34 @@
+import torch
+from torch.optim import Adam
+
+from lightning_fabric import Fabric
+from lightning_fabric.strategies import FSDPStrategy
+
+
+def _custom_auto_wrap_policy(module, recurse, unwrapped_params: int, min_num_params: int = int(1e8)) -> bool:
+    return unwrapped_params >= 2
+
+
+def main():
+    strategy = FSDPStrategy(auto_wrap_policy=_custom_auto_wrap_policy)
+    fabric = Fabric(accelerator="cuda", devices=2, strategy=strategy)
+    fabric.launch()
+
+    model = torch.nn.Linear(10, 10, bias=False)  # total params: 10 * 10 = 100
+    wrapped_model = fabric.setup_module(model)
+
+    optimizer = Adam(wrapped_model.parameters())
+    optimizer = fabric.setup_optimizers(optimizer)
+
+    output = model(torch.rand(5, 10, device=fabric.device))
+    loss = output.sum()
+    fabric.backward(loss)
+    optimizer.step()
+    optimizer.zero_grad()
+
+    state = {"model": model, "optimizer": optimizer, "loss": loss.item()}
+    fabric.save("lightning_logs/sharded", state)
+
+
+if __name__ == "__main__":
+    main()
